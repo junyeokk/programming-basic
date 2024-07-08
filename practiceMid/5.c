a4ad880cc40fe350f169373db10491e6431ef171
junyeokk <cjh4302@gmail.com>
2024-07-08 09:30:00 +0900

    feat: practiceMid

--- practiceMid/5.c
+++ practiceMid/5.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    int arr1[10];
+    int arr[10];
 
-    for (int i = 0; i < 5; i++) {
-        scanf("%d", &arr1[i]);
-    }
-
-    for (int i = 5; i < 10; i++) {
-        scanf("%d", &arr1[i]);
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
     }
 
     for (int i = 0; i < 10; i++) {
         for (int j = i; j < 10; j++) {
-            if (arr1[i] > arr1[j]) {
-                int temp = arr1[i];
-                arr1[i] = arr1[j];
-                arr1[j] = temp;
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
 
     for (int i = 0; i < 10; i++) {
-        if (arr1[i] != arr1[i - 1]) {
-            printf("%d ", arr1[i]);
+        if (arr[i] != arr[i - 1]) {
+            printf("%d ", arr[i]);
         }
     }
 }
